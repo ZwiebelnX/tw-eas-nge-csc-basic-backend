0a4c1f239b3fc01baed1dc4351ce23574192e1e2
Chen Sicong <sicong.chen@163.com>
2020-10-19 22:29:31 +0800

    [Chen Sicong] init repo

---
+++ src/test/java/com/tw/csc/nge/backend/basicbackend/BasicBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tw.csc.nge.backend.basicbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
