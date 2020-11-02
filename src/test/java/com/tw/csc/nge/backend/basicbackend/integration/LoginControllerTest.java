e7df5dfae9aeaf52c33fa32aea6f5dcd0e1ffa07
Chen Sicong <sicong.chen@163.com>
2020-11-02 18:11:09 +0800

    [Chen Sicong] test: add test ddl-auto config

---
+++ src/test/java/com/tw/csc/nge/backend/basicbackend/integration/LoginControllerTest.java
@@ -0,0 +1,50 @@
+package com.tw.csc.nge.backend.basicbackend.integration;
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.tw.csc.nge.backend.basicbackend.IntegrationTest;
+import com.tw.csc.nge.backend.basicbackend.model.dto.LoginDto;
+import com.tw.csc.nge.backend.basicbackend.model.dto.UserDto;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@IntegrationTest
+public class LoginControllerTest{
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Test
+    public void should_login_when_post_login_given_correct_info() throws Exception{
+        UserDto userDTO = UserDto.builder().nickname("Chen").email("sicong.chen@163.com").password("12345678").realName(
+                "ChenSicong").phone("15812418818").build();
+        mockMvc.perform(post("/users").contentType(MediaType.APPLICATION_JSON)
+                                      .content(objectMapper.writeValueAsString(userDTO)))
+               .andExpect(status().isCreated())
+               .andExpect(jsonPath("$.id").exists());
+
+        LoginDto loginDto = LoginDto.builder().loginName("sicong.chen@163.com").password("12345678").build();
+        mockMvc.perform(post("/login").contentType(MediaType.APPLICATION_JSON)
+                                      .content(objectMapper.writeValueAsString(loginDto)))
+               .andExpect(status().isOk())
+               .andExpect(jsonPath("$.id").exists())
+               .andExpect(jsonPath("$.nickname").exists());
+    }
+
+    @Test
+    public void should_fail_to_login_when_post_login_given_incorrect_info() throws Exception{
+        LoginDto loginDto = LoginDto.builder().loginName("sicong.chen@163.com").password("xxxx").build();
+        mockMvc.perform(post("/login").contentType(MediaType.APPLICATION_JSON)
+                                      .content(objectMapper.writeValueAsString(loginDto)))
+               .andExpect(status().isUnauthorized())
+               .andExpect(jsonPath("$.errorCode").value("40101"));
+    }
+}
