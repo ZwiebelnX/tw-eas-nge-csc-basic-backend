4fb8d5eb0875279aee3d2c8e8c33c3897073b9f5
Chen Sicong <sicong.chen@163.com>
2020-11-03 12:07:47 +0800

    [Chen Sicong] refactor: adjust user login logic

--- src/main/java/com/tw/csc/nge/backend/basicbackend/service/LoginService.java
+++ src/main/java/com/tw/csc/nge/backend/basicbackend/service/LoginService.java
@@ -9,16 +9,15 @@ import com.tw.csc.nge.backend.basicbackend.repository.UserRepo;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpSession;
-import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Service
 public class LoginService{
 
     private final UserRepo userRepo;
 
-    private final Map<Long, String> userMap = new HashMap<>();
-
+    private final Map<Long, HttpSession> userMap = new ConcurrentHashMap<>();
 
     public LoginService(UserRepo userRepo){
         this.userRepo = userRepo;
@@ -26,19 +25,26 @@ public class LoginService{
 
     public UserDto userLogin(LoginDto loginDto, HttpSession httpSession){
         UserPo userPo = userRepo.getByNicknameOrEmail(loginDto.getLoginName(), loginDto.getLoginName());
-        if (userPo == null || !userPo.getPassword().equals(loginDto.getPassword())){
+        if(userPo == null || !userPo.getPassword().equals(loginDto.getPassword())){
             throw new BusinessException(BusinessExceptionType.LOGIN_INFO_ILLEGAL);
         }
 
-        this.userMap.put(userPo.getId(), httpSession.getId());
+        userMap.compute(userPo.getId(), (key, value) -> {
+            if(value != null){
+                value.invalidate();
+            }
+            return httpSession;
+        });
+        UserDto userDto = UserDto.builder()
+                                 .id(String.valueOf(userPo.getId()))
+                                 .email(userPo.getEmail())
+                                 .nickname(userPo.getNickname())
+                                 .phone(userPo.getPhone())
+                                 .realName(userPo.getRealName())
+                                 .build();
+        httpSession.setAttribute("userInfo", userDto);
 
-        return UserDto.builder()
-                      .id(String.valueOf(userPo.getId()))
-                      .email(userPo.getEmail())
-                      .nickname(userPo.getNickname())
-                      .phone(userPo.getPhone())
-                      .realName(userPo.getRealName())
-                      .build();
+        return userDto;
     }
 
 
